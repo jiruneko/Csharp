e4871564f0765482b99458ad07e9b6719c567494
jiruneko <oohikaru310@gmail.com>
2025-03-09 07:57:26 +0900

    first commit

---
+++ HelloWorldApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int someValue = GetValue();
+
+            if (someValue > 100)
+            {
+                Console.WriteLine(someValue);
+            }
+        }
+
+        private static int GetValue()
+        {
+            return 150; // 例: 150 を返す
+        }
+    }
+}
---
+++ Sample207/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample207
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("サイコロの目（1〜6）：");
+            int dice = int.Parse(Console.ReadLine());
+
+            if (1 <= dice && dice <= 6)
+            {
+                if (dice == 2 || dice == 4 || dice == 6)
+                {
+                    Console.WriteLine("丁です。");
+                }
+                else
+                {
+                    Console.WriteLine("半です。");
+                }
+            }
+            else
+            {
+                Console.WriteLine("範囲外の数値です。");
+            }
+        }
+    }
+}
